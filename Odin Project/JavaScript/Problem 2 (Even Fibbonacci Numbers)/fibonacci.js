b00d89ac99210045dd2f26e4bece0c7f79161782
JustChillin <devel@justchillin21.com>
2016-04-15 08:47:46 -0700

    completed problem with nested if statements and using the array to store the values.

--- Odin Project/JavaScript/Problem 2 (Even Fibbonacci Numbers)/fibonacci.js
+++ Odin Project/JavaScript/Problem 2 (Even Fibbonacci Numbers)/fibonacci.js
@@ -1,8 +1,14 @@
-var max=10;
-var sum=0;
-for(var i=0;i<max;i+=sum){
-    last=sum;
-    sum=last+i;
-    console.log(last,sum);
+var max=50;     //Sets the maximum loop iterations
+var maxsum=4000000; //Sets the largest number that can be included
+var sum=0;          //initializes sum variable to 0
+var f=[];           //initializes array to store values
+f[0]=0;             //sets first two values to 0 and 1 needed for fibonacci formula
+f[1]=1;
+for(var i=2;i<max;i++){
+    f[i]=f[i-1]+f[i-2];                 //  Fibonacci formula
+    if (!(f[i]%2)&&(f[i]<maxsum)) {     //If the value is even !(%2) and less than the maximum value
+//        console.log("Even",i,f[i]);   //Check to see what the actual value is
+        sum=sum+f[i];   //Sums up the even values
+    }
 }
-console.log(sum);
+console.log(sum);       //Returns the Answer
